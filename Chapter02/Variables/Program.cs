b0131cf15a80ffe060ca6ef0addcd6bee05c5564
dzenor <dzenisredzovic12@gmail.com>
2023-03-21 20:45:02 +0100

    Work on chapter 02

---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml;
+
+object heigh = 1.88;
+object name = "Amir";
+Console.WriteLine($"{name} is {heigh} metres tall.");
+
+//int length1 = name.Length;
+int length2 = ((string)name).Length;
+Console.WriteLine($"{name} has {length2} characters");
+
+var population = 67_000_000; // 67 million in UK
+var weight = 1.88; // in kilograms
+var price = 4.99M; // in pounds sterling
+var fruit = "Apples"; // strings use double-quotes
+var letter = 'Z'; // chars use single-quotes
+var happy = true; // Booleans have value of true or false
+
+// good use of var because it avoids the repeated type
+// as shown in the more verbose second statement
+var xml1 = new XmlDocument(); // C# 3 and later
+XmlDocument xml2 = new XmlDocument(); // all C# versions
+
+// bad use of var because we cannot tell the type, so we
+// should use a specific type declaration as shown in
+// the second statement
+var file1 = File.CreateText("something1.txt");
+StreamWriter file2 = File.CreateText("something2.txt");
+Console.WriteLine($"default(int)={default(int)}");
+Console.WriteLine($"default(bool)={default(bool)}");
+Console.WriteLine($"default(DateTime)={default(DateTime)}");
+Console.WriteLine($"default(string)={default(string)}");
+
+int number = 13;
+Console.WriteLine($"number has been set to: {number} ");
+number = default;
+Console.WriteLine($"number has been reset to its default: {number}");
