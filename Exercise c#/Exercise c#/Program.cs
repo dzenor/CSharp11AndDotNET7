6de7d9c6ef513a2c0e246964385c06a118adb206
dzenor <dzenisredzovic12@gmail.com>
2023-03-26 00:15:44 +0100

    exercises c#

--- Exercise c#/Exercise c#/Program.cs
+++ Exercise c#/Exercise c#/Program.cs
@@ -145,36 +145,36 @@
 //}
 
 
-public class Exercise5
-{
-    public static void Main()
-    {
-        string str;
-        int i, wrd, l;
+//public class Exercise5
+//{
+//    public static void Main()
+//    {
+//        string str;
+//        int i, wrd, l;
 
-        Console.Write("\n\nCount the total number of words in a string :\n");
-        Console.Write("------------------------------------------------------\n");
-        Console.Write("Input the string : ");
-        str = Console.ReadLine();
+//        Console.Write("\n\nCount the total number of words in a string :\n");
+//        Console.Write("------------------------------------------------------\n");
+//        Console.Write("Input the string : ");
+//        str = Console.ReadLine();
 
-        l = 0;
-        wrd = 1;
+//        l = 0;
+//        wrd = 1;
 
-        /* loop till end of string */
-        while (l <= str.Length - 1)
-        {
-            /* check whether the current character is white space or new line or tab character*/
-            if (str[l]==' ' || str[l]=='\n' || str[l]=='\t')
-            {
-                wrd++;
-            }
+//        /* loop till end of string */
+//        while (l <= str.Length - 1)
+//        {
+//            /* check whether the current character is white space or new line or tab character*/
+//            if (str[l]==' ' || str[l]=='\n' || str[l]=='\t')
+//            {
+//                wrd++;
+//            }
 
-            l++;
-        }
+//            l++;
+//        }
 
-        Console.Write("Total number of words in the string is : {0}\n", wrd);
-    }
-}
+//        Console.Write("Total number of words in the string is : {0}\n", wrd);
+//    }
+//}
 
 
 
