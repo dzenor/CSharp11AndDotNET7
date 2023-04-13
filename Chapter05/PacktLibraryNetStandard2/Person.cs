834bc6dab4a36e20a6a2f8e9aebee4e6b9963585
dzenor <dzenisredzovic12@gmail.com>
2023-04-13 20:40:53 +0200

    new

--- Chapter05/PacktLibraryNetStandard2/Person.cs
+++ Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -13,4 +13,68 @@ public class Person : Object
     public WondersOfTheAncientWorld FavoriteAncientWonder;
 
     public List<Person> Children = new List<Person> ();
+
+    public const string Species = "Homo Sapiens";
+
+    public readonly string HomePlanet = "Earth";
+    public readonly DateTime Instantiated;
+
+    public Person()
+    {
+        Name = "Unknown";
+        Instantiated = DateTime.Now;
+    }
+
+    public Person(string initialName, string homePlanet)
+    {
+        Name = initialName;
+        HomePlanet = homePlanet;
+        Instantiated = DateTime.Now;
+    }
+
+    public void WriteConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+
+    public string GetOrigin()
+    {
+        return $"{Name} was born on {HomePlanet}.";
+    }
+
+
+    public (string, int) GetFruit()
+    {
+        return ("Apples", 5);
+    }
+
+    public (string Name, int Number) GetNamedFruit()
+    {
+        return (Name: "Apples", Number: 5);
+    }
+
+    public string SayHello()
+    {
+        return $"{Name} says 'Hello!'";
+    }
+
+    public string SayHello(string name)
+    {
+        return $"{Name} says Hello, {name}";
+    }
+
+    public string OptionalParameters(string command = "Run!",
+    double number = 0.0, bool active = true)
+    {
+        return string.Format(
+        format: "command is {0}, number is {1}, active is {2}",
+        arg0: command,
+        arg1: number,
+        arg2: active);
+    }
+
+
+
+
+
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -5,7 +5,7 @@ Person bob = new Person();
 WriteLine(bob.ToString());
 
 bob.Name = "Bob Smith";
-bob.DateOfBirth = new DateTime(1965, 12, 22);
+bob.DateOfBirth = new DateTime(1965, 12, 22); 
 Console.OutputEncoding = Encoding.UTF8;
 
 WriteLine(format: "{0} was born on {1:dddd,d MMMM yyyy}",
@@ -55,3 +55,43 @@ gerrierAccount.Balance = 98;
 WriteLine(format: "{0} earned {1:C} interest.",
     arg0: gerrierAccount.AccountName,
     arg1: gerrierAccount.Balance*BankAccount.InterestRate);
+
+WriteLine($"{bob.Name} is a {Person.Species}");
+WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
+
+//Console.ReadLine();
+Person blankPerson = new();
+
+WriteLine(format:
+    "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+    arg0:blankPerson.Name,
+    arg1:blankPerson.HomePlanet,
+    arg2:blankPerson.Instantiated
+    );
+
+WriteLine(format:
+    "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+    arg0: bob.Name,
+    arg1: bob.HomePlanet,
+    arg2: bob.Instantiated
+    );
+
+Person gunny = new(initialName:"Gunny",homePlanet:"Mars");
+WriteLine(format:
+     "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+      arg0: gunny.Name,
+    arg1: gunny.HomePlanet,
+    arg2: gunny.Instantiated
+    );
+
+bob.WriteConsole();
+WriteLine(bob.GetOrigin());
+
+(string,int) fruit = bob.GetFruit();
+WriteLine($"{fruit.Item1},{fruit.Item2} there are");
+var fruitNamed = bob.GetNamedFruit();
+WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
+
+WriteLine(bob.SayHello());
+WriteLine(bob.SayHello("Dzenis"));
+WriteLine(bob.OptionalParameters());
