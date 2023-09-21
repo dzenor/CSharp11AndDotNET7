dbe5e186d4db3ca30082520bb65e31fab7522890
Dzenis Redzovic <dzenisredzovic12@gmail.com>
2023-09-21 23:25:12 +0200

    started new project for exercise

---
+++ ContactManager/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using ContactManager.Data;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ContactController(ApplicationDbContext db)
+        {
+           _db=db;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(Contacts contact)
+        {
+          
+            _db.Contacts.Add(contact);
+            _db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
